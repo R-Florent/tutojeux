4d6748d96d78a86ff5090f56ffc323fa33d7827f
Florent <florent.real@viacesi.fr>
2023-06-18 17:17:00 +0200

    création du porjet + ajout de tout les asset pour le tuto du jeux vidéo

---
+++ main.py
@@ -0,0 +1,26 @@
+import pygame
+pygame.init()
+
+pygame.display.set_caption("Le jeux qu'il est trop bien ")
+screen = pygame.display.set_mode((1080,720))
+
+#importe et ccharger le background du jeux
+background = pygame.image.load('PygameAssets-main/bg.jpg')
+
+running = True
+
+while running:
+
+    #appliquer le back gound du jeux
+    screen.blit(background,(0,-200))
+
+    #mettre à jour l'écran
+    pygame.display.flip()
+
+    #si le joueur ferme cette fenetre
+    for event in pygame.event.get():
+        #que l'évent est fermeture de fennetre
+        if event.type == pygame.QUIT:
+            runnig = False
+            pygame.quit()
+            print("jeux a été quitter")
