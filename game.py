2080c05de09a1bd0549a55151394ac6d1e21e0ee
Florent <florent.real@viacesi.fr>
2023-06-26 16:40:38 +0200

    crorrection de bug

--- game.py
+++ game.py
@@ -2,6 +2,8 @@ from class_Player import Player
 
 class Game:
 
+    player = Player
+
     def __init__(self):
 
         self.player = Player
--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 import pygame
+from class_Player import Player
+from game import Game
+
+
+
 pygame.init()
 
 pygame.display.set_caption("Le jeux qu'il est trop bien ")
@@ -7,12 +12,15 @@ screen = pygame.display.set_mode((1080,720))
 #importe et ccharger le background du jeux
 background = pygame.image.load('PygameAssets-main/bg.jpg')
 
+
 running = True
 
 while running:
 
     #appliquer le back gound du jeux
     screen.blit(background,(0,-200))
+
+    screen.blit(Game.player.image, Game.player.rect)
 
     #mettre à jour l'écran
     pygame.display.flip()
