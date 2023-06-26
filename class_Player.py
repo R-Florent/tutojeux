449ae6fdba5d8d93fc4e1f568389574b7b096cab
Florent <florent.real@viacesi.fr>
2023-06-26 16:32:11 +0200

    ajoute de la calsse player et game qui permet respectivement d'afficher le sprite du joeuer et de l'instanciée dans la fentre game

---
+++ class_Player.py
@@ -0,0 +1,15 @@
+import pygame
+
+
+class Player(pygame.sprite.Sprite):
+
+    def __init__(self):
+        super().__init__()
+        self.health = 100
+        self.max_health =100
+        self.attack = 10
+        self.velocity = 5
+        self.image = pygame.image.load('pygrameAssets-main/player.png')
+        self.rect = self.image.get_rect()
+        self.rect.x = 400
+        self.rect.y = 500
---
+++ game.py
@@ -0,0 +1,7 @@
+from class_Player import Player
+
+class Game:
+
+    def __init__(self):
+
+        self.player = Player
