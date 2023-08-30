d11387037ba808c4205d74440d36d00e82d80111
RF <123410951+R-Florent@users.noreply.github.com>
2023-08-30 23:28:22 +0200

    Update class_Player.py

--- class_Player.py
+++ class_Player.py
@@ -8,7 +8,7 @@ class Player(pygame.sprite.Sprite):
         self.health = 100
         self.max_health = 100
         self.attack = 10
-        self.velocity = 5
+        self.velocity = 3
         self.all_projectiles = pygame.sprite.Group()
         self.image = pygame.image.load('PygameAssets-main/player.png')
         print(self.image)
