65154b95a7f82961d2e9d7aa50bd44f80c79bd81
Flo <florent.real@viacesi.fr>
2023-09-22 16:21:43 +0200

    Projectile: affichage du menu

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import pygame
 from pygame.time import Clock
-
+import math
 from game import Game
 
 pygame.init()
@@ -8,11 +8,16 @@ pygame.init()
 pygame.display.set_caption("Le jeux qu'il est trop bien ")
 screen = pygame.display.set_mode((1080, 720))
 
-# importe et ccharger le background du jeux
+# importe et charger le background du jeux et +
 background = pygame.image.load('PygameAssets-main/bg.jpg')
+banner = pygame.image.load('PygameAssets-main/banner.png')
+banner = pygame.transform.scale(banner,(500,500))
+banner_rect = banner.get_rect()
+banner_rect.x = math.ceil(screen.get_width()/4)
 
 clock = pygame.time.Clock()
 
+#chager notre jeux
 game = Game()
 
 running = True
@@ -29,6 +34,9 @@ while running:
     #vérife si le jeux et en marche
     if game.is_playing:
         game.update(screen)
+    else:
+        screen.blit(banner, banner_rect)
+
 
     # mettre à jour l'écran
     pygame.display.flip()
@@ -42,11 +50,11 @@ while running:
             print("jeux a été quitter")
         # implémentation des touche de
         elif event.type == pygame.KEYDOWN:
-            self.pressed[event.key] = True
+            game.pressed[event.key] = True
 
             # Permet de savoir quand le joeur veut tirait un proctile avec la touche espace
             if event.key == pygame.K_SPACE:
-                self.player.launch_projectile()
+                game.player.launch_projectile()
 
         elif event.type == pygame.KEYUP:
-            self.pressed[event.key] = False
+            game.pressed[event.key] = False
